de263b644bf4f07c4738c1e7a11c93ac9f3602fa
bhuynhdev <bhuynhdev@gmail.com>
2024-11-25 18:53:25 -0500

    DaisyUI and drawer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="drawer drawer-end">
+  <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content">
+    <!-- Page content here -->
+    <label for="my-drawer-4" class="drawer-button btn btn-primary"
+      >Open drawer</label
+    >
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"
+    ></label>
+    <div
+      class="bg-base-200 text-base-content min-h-full w-80 p-4 flex flex-col gap-2 p-4"
+    >
+      <p class="border border-red-500">Hello</p>
+      <p class="border border-green-500">World</p>
+    </div>
+  </div>
+</div>
